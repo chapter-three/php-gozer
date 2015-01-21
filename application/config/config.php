36a04577f5bccb3a2416f9ec67c1fe614984ad04
Jim McGowen <jim@builtbyhq.com>
2015-01-21 13:26:08 -0700

    Configurable OAuth2.

--- application/config/config.php
+++ application/config/config.php
@@ -1,23 +1,28 @@
 <?php
 
-// Gozer
+// *** Gozer ***
 define('BASE_PATH', dirname(dirname(dirname(__FILE__))));
 define('ROUTES_FILE', BASE_PATH . '/application/config/routes.json');
 define('ENV', 'dev'); // TODO: This is server and install specific and so should be elsewhere.
 
-// API/Webservice and OAuth
+// *** API/Webservice and OAuth ***
 define('API_USE_OAUTH', true);
 define('OAUTH_STORAGE_TYPE', 'pdo');
+// Default is 1 hour (3600 seconds)
 define('OAUTH_TOKEN_LIFETIME', 3600);
-define('OAUTH_GRANT_TYPES', serialize(array('client_credentials')));
+// Any combination of 'authorization_code', 'client_credentials', 'password'
+define('OAUTH_GRANT_TYPES', serialize(array('client_credentials', 'authorization_code')));
+define('OAUTH_ISSUE_REFRESH_TOKENS', true);
+// Default is 14 days (2419200 seconds).
+define('OAUTH_REFRESH_TOKEN_LIFETIME', 2419200);
 
-// Twig
+// *** Twig ***
 define('TWIG_TEMPLATE_DIR', BASE_PATH . '/application/views');
 define('TWIG_CACHE_DIR', BASE_PATH . '/system/cache/twig');
 
-// Doctrine
+// *** Doctrine ***
 define('DOCTRINE_ENTITIES_DIR', BASE_PATH . '/application/models');
 define('DOCTRINE_DB_DRIVER', 'pdo_mysql');
-define('DOCTRINE_DB_NAME', 'doctrine');
-define('DOCTRINE_DB_USER', 'root');
-define('DOCTRINE_DB_PASSWORD', 'mysqlr00t');
+define('DOCTRINE_DB_NAME', '');
+define('DOCTRINE_DB_USER', '');
+define('DOCTRINE_DB_PASSWORD', '');
--- system/src/CoreAPI.php
+++ system/src/CoreAPI.php
@@ -5,30 +5,31 @@ namespace Gozer\Core;
 /**
  * Base class for all api controllers.
  * 
- * Has built in OAuth2 authentication via the bshaffer/oauth2-server-php library.
- * (http://bshaffer.github.io/oauth2-server-php-docs/) 
+ * Has built in OAuth2 authentication via the bshaffer/oauth2-server-php library. 
  * Pass true to the constructor to enable OAuth2.
  * 
- * TODO: Use Doctrine storage?
- *
+ * TODO: Add JWT Bearer support. See http://bshaffer.github.io/oauth2-server-php-docs/grant-types/jwt-bearer/
+ * 
  * @author Jim McGowen
  *
  */
 abstract class CoreAPI extends Core
 {
 	protected $oauthServer;
+	private $useOAuth2 = false;
 	
-	public function __construct() 
-	{
-		if (API_USE_OAUTH) 
-		{
-			$this->initOAuth2();
-
+	public function __construct($useOAuth2 = false) {
+		$this->useOAuth2 = $useOAuth2;
+		
+		if ($this->useOAuth2) {
+			$this->initOAth2();
+			
 			// Don't check for authorization when requesting a token
 			$temp = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
 			if ($temp[count($temp) - 1] != 'authorize') {
 				// Check for a valid token
 				if (!$this->oauthServer->verifyResourceRequest(\OAuth2\Request::createFromGlobals())) {
+					// Not authorized!
 					$this->oauthServer->getResponse()->send();
 					die;
 				}
@@ -36,44 +37,63 @@ abstract class CoreAPI extends Core
 		}
 	}
 	
-	private function initOAuth2() 
-	{
-		$storage = null;
-		switch (OAUTH_STORAGE_TYPE) {
-			case 'pdo':
-				$storage = new \OAuth2\Storage\Pdo(array(
-					'dsn' => 'mysql:dbname=' . DOCTRINE_DB_NAME . ';host=localhost',
-					'username' => DOCTRINE_DB_USER,
-					'password' => DOCTRINE_DB_PASSWORD
-				));
-				break;
-			default:
-				throw new \Exception('Invalid OAuth2 storage type');
+	private function initOAth2() {
+		$storage = new \OAuth2\Storage\Pdo(array(
+			'dsn' => 'mysql:dbname=' . DOCTRINE_DB_NAME . ';host=localhost',
+			'username' => DOCTRINE_DB_USER,
+			'password' => DOCTRINE_DB_PASSWORD
+		));
+
+		//$storage->checkRestrictedGrantType()
+		
+		$grantTypes = unserialize(OAUTH_GRANT_TYPES);
+
+		$options = array();
+		
+		if (in_array('implicit', $grantTypes)) {
+			$options['allow_implicit'] = true;
 		}
 		
-		$opts = array(
-			'access_lifetime' => OAUTH_TOKEN_LIFETIME
-		);
+		if (OAUTH_ISSUE_REFRESH_TOKENS) {
+			$options['always_issue_new_refresh_token'] = true;
+			$options['refresh_token_lifetime'] = OAUTH_REFRESH_TOKEN_LIFETIME;
+		}
 
-		$this->oauthServer = new \OAuth2\Server($storage, $opts);
+		$this->oauthServer = new \OAuth2\Server($storage, $options);
 		
-		foreach (unserialize(OAUTH_GRANT_TYPES) as $grantType) {
+		foreach ($grantTypes as $grantType) {
 			switch ($grantType) {
+				case 'authorization_code':
+					// 3-legged authentication
+					// See http://bshaffer.github.io/oauth2-server-php-docs/grant-types/authorization-code/
+					$this->oauthServer->addGrantType(new \OAuth2\GrantType\AuthorizationCode($storage));
+					break;
 				case 'client_credentials':
+					// Uses client_id and client_secret.
+					// Example request:
+					//      curl -u TestClient:TestSecret https://api.mysite.com/token -d 'grant_type=client_credentials'
+					//   or:
+					//      curl https://api.mysite.com/authorize -d 'grant_type=client_credentials&client_id=TestClient&client_secret=TestSecret'
 					$this->oauthServer->addGrantType(new \OAuth2\GrantType\ClientCredentials($storage));
 					break;
-				case 'authorization_code':
-					$this->oauthServer->addGrantType(new \OAuth2\GrantType\AuthorizationCode($storage));
+				case 'password':
+					// Uses username and password.
+					// Example request:
+					//      curl -u TestClient:TestSecret https://api.mysite.com/token -d 'grant_type=password&username=bshaffer&password=brent123'
+					//   or:
+					//      curl https://api.mysite.com/token -d 'grant_type=password&client_id=TestClient&username=bshaffer&password=brent123'
+					$this->oauthServer->addGrantType(new \OAuth2\GrantType\UserCredentials($storage));
 					break;
 			}
 		}
+		
+		if (OAUTH_ISSUE_REFRESH_TOKENS) {
+			$this->oauthServer->addGrantType(new \OAuth2\GrantType\RefreshToken($storage));
+		}
 	}
 	
-	public function getOAuth2Token() 
-	{
+	public function getOAuth2Token() {
 		// Respond with a new token
 		$this->oauthServer->handleTokenRequest(\OAuth2\Request::createFromGlobals())->send();
 	}
-	
-	// TODO: Create a helper function for creating a user account in oauth_clients
 }
