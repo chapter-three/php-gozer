7f12133f22ec5b5278e6ea546d2a43759317789c
Jim McGowen <jim@builtbyhq.com>
2015-01-21 14:37:40 -0700

    Removed support for authorization_code and implicit grant types. Too complicated to implement right now. Will add them at a future time.

--- application/config/config.php
+++ application/config/config.php
@@ -10,8 +10,8 @@ define('API_USE_OAUTH', true);
 define('OAUTH_STORAGE_TYPE', 'pdo');
 // Default is 1 hour (3600 seconds)
 define('OAUTH_TOKEN_LIFETIME', 3600);
-// Any combination of 'authorization_code', 'client_credentials', 'password'
-define('OAUTH_GRANT_TYPES', serialize(array('client_credentials', 'authorization_code')));
+// Only supports 'client_credentials' and 'password' for now.
+define('OAUTH_GRANT_TYPES', serialize(array('client_credentials')));
 define('OAUTH_ISSUE_REFRESH_TOKENS', true);
 // Default is 14 days (2419200 seconds).
 define('OAUTH_REFRESH_TOKEN_LIFETIME', 2419200);
--- system/src/CoreAPI.php
+++ system/src/CoreAPI.php
@@ -25,7 +25,7 @@ abstract class CoreAPI extends Core
 			$this->initOAth2();
 			
 			// Don't check for authorization when requesting a token
-			$temp = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
+			$temp = explode('/', trim($_SERVER['PATH_INFO'], '/'));
 			if ($temp[count($temp) - 1] != 'authorize') {
 				// Check for a valid token
 				if (!$this->oauthServer->verifyResourceRequest(\OAuth2\Request::createFromGlobals())) {
@@ -36,7 +36,20 @@ abstract class CoreAPI extends Core
 			}
 		}
 	}
-	
+
+	/**
+	 * Only the client_credentials and password grant_types are currently supported.
+	 * Will add support for authorization_code and implicit when needed.
+	 * 
+	 * authorization_code (future support)
+	 *      3-legged authentication. See http://bshaffer.github.io/oauth2-server-php-docs/grant-types/authorization-code/
+	 * client_credentials
+	 *      data: client_id and client_secret
+	 * password
+	 *      data: username and password
+	 * implicit (future support)
+	 *      Allows implicit 
+	 */
 	private function initOAth2() {
 		$storage = new \OAuth2\Storage\Pdo(array(
 			'dsn' => 'mysql:dbname=' . DOCTRINE_DB_NAME . ';host=localhost',
@@ -50,31 +63,39 @@ abstract class CoreAPI extends Core
 
 		$options = array();
 		
+		/*
 		if (in_array('implicit', $grantTypes)) {
+			// 'implicit' requires 'authorization_code'
+			if (!in_array('authorization_code', $grantTypes)) {
+				$grantTypes[] = 'authorization_code';
+			}
 			$options['allow_implicit'] = true;
 		}
+		*/
 		
 		if (OAUTH_ISSUE_REFRESH_TOKENS) {
 			$options['always_issue_new_refresh_token'] = true;
 			$options['refresh_token_lifetime'] = OAUTH_REFRESH_TOKEN_LIFETIME;
 		}
 
-		$this->oauthServer = new \OAuth2\Server($storage, $options);
+		$grants = array();
 		
 		foreach ($grantTypes as $grantType) {
 			switch ($grantType) {
+				/*
 				case 'authorization_code':
 					// 3-legged authentication
 					// See http://bshaffer.github.io/oauth2-server-php-docs/grant-types/authorization-code/
 					$this->oauthServer->addGrantType(new \OAuth2\GrantType\AuthorizationCode($storage));
 					break;
+				*/
 				case 'client_credentials':
 					// Uses client_id and client_secret.
 					// Example request:
 					//      curl -u TestClient:TestSecret https://api.mysite.com/token -d 'grant_type=client_credentials'
 					//   or:
 					//      curl https://api.mysite.com/authorize -d 'grant_type=client_credentials&client_id=TestClient&client_secret=TestSecret'
-					$this->oauthServer->addGrantType(new \OAuth2\GrantType\ClientCredentials($storage));
+					$grants[] = new \OAuth2\GrantType\ClientCredentials($storage);
 					break;
 				case 'password':
 					// Uses username and password.
@@ -82,16 +103,31 @@ abstract class CoreAPI extends Core
 					//      curl -u TestClient:TestSecret https://api.mysite.com/token -d 'grant_type=password&username=bshaffer&password=brent123'
 					//   or:
 					//      curl https://api.mysite.com/token -d 'grant_type=password&client_id=TestClient&username=bshaffer&password=brent123'
-					$this->oauthServer->addGrantType(new \OAuth2\GrantType\UserCredentials($storage));
+					$grants[] = new \OAuth2\GrantType\UserCredentials($storage);
 					break;
 			}
 		}
 		
 		if (OAUTH_ISSUE_REFRESH_TOKENS) {
-			$this->oauthServer->addGrantType(new \OAuth2\GrantType\RefreshToken($storage));
+			$grants[] = new \OAuth2\GrantType\RefreshToken($storage);
 		}
+
+		$this->oauthServer = new \OAuth2\Server($storage, $options, $grants);
 	}
 	
+	/**
+	 * This is the client authorize endpoint.
+	 * Requires a route like so:
+	 * {
+	 *      "url":			"/api/authorize",
+	 *      "controller":	"<NameOfYourController>",
+	 *      "action":		"getOAuth2Token"
+	 * }
+	 * 
+	 * Then the client would post to http(s)://<yourdomain.com>/api/authorize/ 
+	 * with data appropriate for the grant type to get an access_token for use 
+	 * in subsequent calls (defined in your controller).
+	 */
 	public function getOAuth2Token() {
 		// Respond with a new token
 		$this->oauthServer->handleTokenRequest(\OAuth2\Request::createFromGlobals())->send();
