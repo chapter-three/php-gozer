396584632738baf1d20629c5e99cfcca5048723c
Jim McGowen <jim@builtbyhq.com>
2016-06-02 09:37:22 -0600

    Removed JSON_UNESCAPED_UNICODE

--- src/CoreAPIResponseJSON.php
+++ src/CoreAPIResponseJSON.php
@@ -5,7 +5,7 @@ namespace Gozer\Core;
 class CoreAPIResponseJSON implements CoreAPIResponse {
 	public function respond($data) {
 		header("Content-Type: application/json");
-		$json = json_encode($data, JSON_UNESCAPED_UNICODE);
+		$json = json_encode($data);
 		if ($json === false) {
 			echo('JSON encodeing error ' . json_last_error() . ': ' . json_last_error_msg());
 			echo('data:');
